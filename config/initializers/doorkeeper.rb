01e75a5f20484efebf331fbff2695e38e86cedf2
Tatsujin Chin <c910335@gmail.com>
2016-10-17 14:11:28 +0800

    Enable implicit flow & refresh token

--- config/initializers/doorkeeper.rb
+++ config/initializers/doorkeeper.rb
@@ -42,7 +42,7 @@ Doorkeeper.configure do
   # reuse_access_token
 
   # Issue access tokens with refresh token (disabled by default)
-  # use_refresh_token
+  use_refresh_token
 
   # Provide support for an owner to be assigned to each registered application (disabled by default)
   # Optional parameter confirmation: true (default false) if you want to enforce ownership of
@@ -97,7 +97,7 @@ Doorkeeper.configure do
   #   http://tools.ietf.org/html/rfc6819#section-4.4.2
   #   http://tools.ietf.org/html/rfc6819#section-4.4.3
   #
-  # grant_flows %w(authorization_code client_credentials)
+  grant_flows %w(authorization_code implicit client_credentials)
 
   # Under some circumstances you might want to have applications auto-approved,
   # so that the user skips the authorization step.
